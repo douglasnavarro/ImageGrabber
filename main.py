bdd807e35b394b63dc5894148fdb6e32f1fd92b5
Douglas <douglasnavarro94@gmail.com>
2018-03-24 13:34:21 -0300

    implemented update_current_image and update_preview_widget

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from PIL import ImageTk, Image
 
 MINICAP = os.getcwd() + "\\bin\\MiniCap.exe"
 
-def grab_image(name):
-    return subprocess.call([MINICAP, "-captureregselect", "-exit", "-save", r"..\images\\" + name])
 
 class GUI:
     def __init__(self, master):
@@ -21,6 +19,7 @@ class GUI:
         self.preview = Label(master, image = self.previewImg)
 
         self.ssButton   = Button(master, text="Capturar ibagem")
+        self.ssButton.bind('<Button-1>', self.update_preview_widget)
         self.saveButton = Button(master, text='Salvar ibagem')        
 
         self.iframe = Frame(master, bd=2, relief=RIDGE)
@@ -35,6 +34,19 @@ class GUI:
         self.pathLabel.pack(side=LEFT, padx=5, pady=5)
         self.pathEntry.pack(padx=5, pady=5, fill=X)
         self.saveButton.pack(pady=5)
+
+    @classmethod
+    def update_current_image(self):
+        status = subprocess.call([MINICAP, "-captureregselect", "-exit", "-save", "..\\preview.png"])
+        return status
+    
+    def update_preview_widget(self, event):
+        status = self.update_current_image()
+        if(status == 0):
+            self.previewImg = ImageTk.PhotoImage(Image.open(self.pathToPreviewImg))
+            self.preview.configure(image=self.previewImg)
+            self.preview.image = self.previewImg
+        return status
 
 def main():
     root = Tk()
