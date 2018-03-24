52a25fe0ce1d6795c625165770832ed508bbfb78
Douglas <douglasnavarro94@gmail.com>
2018-03-24 10:47:52 -0300

    packed tk methods into gui class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
-from tkinter import *
-from tkinter import ttk
+from tkinter import Tk, Label, Button, Entry, StringVar, Frame, RIDGE, X, LEFT
 from PIL import ImageTk, Image
 
 MINICAP = os.getcwd() + "\\bin\\MiniCap.exe"
@@ -9,32 +8,37 @@ MINICAP = os.getcwd() + "\\bin\\MiniCap.exe"
 def grab_image(name):
     return subprocess.call([MINICAP, "-captureregselect", "-exit", "-save", r"..\images\\" + name])
 
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Eu quero ibagens")
+
+        # Creation of widgets
+        self.path = StringVar()
+        self.pathToPreviewImg = "preview.png"
+        self.previewImg = ImageTk.PhotoImage(Image.open(self.pathToPreviewImg))
+
+        self.preview = Label(master, image = self.previewImg)
+
+        self.ssButton   = Button(master, text="Capturar ibagem")
+        self.saveButton = Button(master, text='Salvar ibagem')        
+
+        self.iframe = Frame(master, bd=2, relief=RIDGE)
+        self.pathLabel = Label(self.iframe, text='Destino: ')
+        self.pathEntry = Entry(self.iframe, textvariable=self.path, bg='white')
+        self.path.set(r"C:\Scripts\Imagens")
+
+        # Layout of widgets
+        self.ssButton.pack(pady=10)
+        self.preview.pack()
+        self.iframe.pack(padx=50,fill=X)
+        self.pathLabel.pack(side=LEFT, padx=5, pady=5)
+        self.pathEntry.pack(padx=5, pady=5, fill=X)
+        self.saveButton.pack(pady=5)
 
 def main():
-
     root = Tk()
-
-    ssButton = Button(root, text="Capturar ibagem")
-    ssButton.pack(pady=10)
-
-    previewImg = ImageTk.PhotoImage(Image.open("preview.png"))
-    preview = Label(root, image = previewImg)
-    preview.pack()
-
-    iframe = Frame(root, bd=2, relief=RIDGE)
-    iframe.pack(padx=50,fill=X)
-
-    pathLabel = Label(iframe, text='Destino: ')
-    pathLabel.pack(side=LEFT, padx=5, pady=5)
-
-    path = StringVar()
-    pathEntry = Entry(iframe, textvariable=path, bg='white')
-    path.set(r'C:\scritps\images\preview.png')
-    pathEntry.pack(padx=5, pady=5, fill=X)
-
-    saveButton = Button(root, text='Salvar ibagem')
-    saveButton.pack(pady=5)
-    
+    my_gui = GUI(root)
     root.mainloop()
 
 main()
